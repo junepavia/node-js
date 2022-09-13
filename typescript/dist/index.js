2c740d436854f0cd53f3784afa3645395f75bd57
hunyo <june.pavia@gmail.com>
2022-09-14 07:58:02 +0800

    typescript functions

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -2,4 +2,13 @@
 ;
 const mySize = 2;
 console.log(mySize);
+function calculateTax(income, taxYear) {
+    if (taxYear < 2022) {
+        return income * 1.2;
+    }
+    else {
+        return income * 1.3;
+    }
+}
+console.log(calculateTax(10000, 2022));
 //# sourceMappingURL=index.js.map
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -2,3 +2,13 @@ const enum size { small, medium, large };
 
 const mySize: size = size.large;
 console.log(mySize)
+
+function calculateTax(income: number, taxYear = 2022): number {
+    if(taxYear < 2022) {
+        return income * 1.2;
+    } else {
+        return income * 1.3;
+    }
+}
+
+console.log(calculateTax(10_000));
