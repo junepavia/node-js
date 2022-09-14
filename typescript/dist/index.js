5ac16378fc8670fea6c1b748d17bd6cbbf23d178
hunyo <june.pavia@gmail.com>
2022-09-14 08:53:51 +0800

    ts intersections

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -1,8 +1,15 @@
 "use strict";
 ;
+const employee = {
+    id: 1,
+    name: '',
+    retire: (date) => {
+        console.log(date);
+    }
+};
 const mySize = 2;
 console.log(mySize);
-function calculateTax(income, taxYear) {
+function calculateTax(income, taxYear = 2022) {
     if (taxYear < 2022) {
         return income * 1.2;
     }
@@ -10,5 +17,13 @@ function calculateTax(income, taxYear) {
         return income * 1.3;
     }
 }
-console.log(calculateTax(10000, 2022));
+console.log(calculateTax(10000));
+function kgToLbs(weight) {
+    if (typeof weight === 'number') {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
 //# sourceMappingURL=index.js.map
